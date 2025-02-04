492ea6b90e075637ee0cc6260763592302b504cc
Jpprignolato <joseprignolato@gmail.com>
2025-02-04 17:28:01 -0300

    Criação da div do dashboard

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -12,6 +12,7 @@ import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
 import { CookieService } from 'ngx-cookie-service';
 import { ChartModule} from 'primeng/chart';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
@@ -30,7 +31,9 @@ import { ChartModule} from 'primeng/chart';
     ToolbarModule,
     CardModule,
     ToastModule,
-    ChartModule
+    ChartModule,
+    // Shared
+    SharedModule,
   ],
   providers: [MessageService, CookieService],
 })
