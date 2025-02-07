879f60af3a204bd03634e617de1e75affbd8514f
Jpprignolato <joseprignolato@gmail.com>
2025-02-06 23:05:12 -0300

    resolvendo memory leak + criação de gráfico de produtos

--- src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
+++ src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { tick } from '@angular/core/testing';
+import { ChartData, ChartOptions } from 'chart.js';
 import { MessageService } from 'primeng/api';
+import { Subject, takeUntil } from 'rxjs';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
 import { ProductsDataTransferService } from 'src/app/services/products/products-data-transfer.service';
 import { ProductsService } from 'src/app/services/products/products.service';
@@ -9,8 +12,12 @@ import { ProductsService } from 'src/app/services/products/products.service';
   templateUrl: './dashboard-home.component.html',
   styleUrls: [],
 })
-export class DashboardHomeComponent implements OnInit {
+export class DashboardHomeComponent implements OnInit, OnDestroy {
+  public destroy$ = new Subject<void>();
   public productList: Array<GetAllProductsResponse> = [];
+
+  public productsChartDatas!: ChartData;
+  public productsChartOptions!: ChartOptions;
 
 
   constructor (
@@ -24,11 +31,14 @@ export class DashboardHomeComponent implements OnInit {
   }
 
   getProductsDatas(): void {
-    this.productsService.getAllProducts().subscribe({
+    this.productsService.getAllProducts()
+    .pipe(takeUntil(this.destroy$))
+    .subscribe({
       next: (response) => {
         if (response.length > 0) {
           this.productList = response;
           this.productsDtService.setProductsDatas(this.productList);
+          this.setProductsChartConfig();
         }
       },
       error: (err) => {
@@ -41,5 +51,66 @@ export class DashboardHomeComponent implements OnInit {
         });
       },
     });
+  }
+
+  setProductsChartConfig(): void {
+    if(this.productList.length > 0){
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+    const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+    this.productsChartDatas = {
+      labels: this.productList.map((element) => element?.name),
+      datasets: [
+        {
+        label: 'Quantidade',
+        backgroundColor: documentStyle.getPropertyValue('--indigo-400'),
+        borderColor: documentStyle.getPropertyValue('--indigo-400'),
+        hoverBackgroundColor: documentStyle.getPropertyValue('--indigo-500'),
+        data: this.productList.map((element) => element?.amount),
+        },
+      ],
+    };
+
+    this.productsChartOptions = {
+      maintainAspectRatio: false,
+      aspectRatio: 0.8,
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor,
+          }
+        }
+      },
+
+      scales: {
+        x: {
+          ticks: {
+            color: textColorSecondary,
+            font: {
+              weight: 500,
+            },
+          },
+          grid: {
+            color: surfaceBorder
+          }
+        },
+        y:{
+          ticks: {
+            color: textColorSecondary,
+          },
+          grid: {
+            color: surfaceBorder
+          }
+        }
+      }
+    }
+  }
+}
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,5 +1,5 @@
 import { CookieService } from 'ngx-cookie-service';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { SignupUserResponse } from 'src/app/models/interfaces/user/SignupUserResponse';
 import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
@@ -7,13 +7,15 @@ import { UserService } from 'src/app/services/user/user.service';
 import { AuthRequest } from 'src/app/models/interfaces/user/auth/AuthRequest';
 import { MessageService } from 'primeng/api';
 import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy {
+  private destroy$ = new Subject<void>();
   loginCard = true;
 
   loginForm = this.formBuilder.group({
@@ -37,7 +39,9 @@ export class HomeComponent {
 
   onSubmitLoginForm(): void {
     if(this.loginForm.value && this.loginForm.valid) {
-      this.userService.authUser(this.loginForm.value as AuthRequest).subscribe({
+      this.userService.authUser(this.loginForm.value as AuthRequest)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
         next: (response) => {
           if(response) {
             this.cookieService.set('USER_INFO', response?.token);
@@ -69,6 +73,7 @@ export class HomeComponent {
     if (this.signupForm.value && this.signupForm.valid) {
       this.userService
         .signupUser(this.signupForm.value as SignupUserRequest)
+        .pipe(takeUntil(this.destroy$))
         .subscribe({
           next: (response) => {
             if (response) {
@@ -93,5 +98,10 @@ export class HomeComponent {
           },
         });
     }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
