fa49e49e65692c5875a47f1ad1c9f1daf56ec389
Jpprignolato <joseprignolato@gmail.com>
2025-02-18 11:58:09 -0300

    Função de vender produto

--- src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
+++ src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
@@ -1,6 +1,9 @@
+import { DialogService } from 'primeng/dynamicdialog';
 import { CookieService } from 'ngx-cookie-service';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ProductFormComponent } from 'src/app/modules/products/components/product-form/product-form.component';
+import { ProductEvent } from 'src/app/models/enums/products/ProductEvent';
 
 @Component({
   selector: 'app-toolbar-navigation',
@@ -8,11 +11,26 @@ import { Router } from '@angular/router';
   styleUrls: []
 })
 export class ToolbarNavigationComponent {
-
-constructor(private cookie: CookieService, private router: Router) {}
+constructor(private cookie: CookieService, private router: Router,
+  private dialogService: DialogService
+) {}
 
 handleLogout(): void {
   this.cookie.delete('USER_INFO');
   this.router.navigate(['/home'])
+  }
+
+handleSaleProduct(): void {
+    const saleProductAction = ProductEvent.SALE_PRODUCT_EVENT
+    this.dialogService.open(ProductFormComponent, {
+      header: ProductEvent.SALE_PRODUCT_EVENT,
+      width: '70%',
+      contentStyle: { overflow: 'auto' },
+      baseZIndex: 10000,
+      maximizable: true,
+      data: {
+        event: {action: saleProductAction}
+      }
+    })
   }
 }
